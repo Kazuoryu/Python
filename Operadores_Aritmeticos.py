0a892394d75e2fab8c723cc49d296b14bae511ba
Diego <ichigo1991@hotmail.es>
2016-11-30 00:01:19 +0000

    Operadores Aritmeticos

---
+++ Operadores_Aritmeticos.py
@@ -0,0 +1,23 @@
+#reales
+real = 0.567
+real = 0.56e-3
+print (real)
+#variables
+a = 26
+b =11.3
+c = 5
+d = 3.5
+#suma
+print (a+b)
+#resta
+print (c-a)
+#multiplicacion
+print (d*c)
+#exponente
+print (c**2)
+#division
+print (c/a)
+#division entera
+print (c/a)
+#modulo
+print (7%3)
